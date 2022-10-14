06cc145ff0f50e0e51d826547ad8a8627cecf38b
Damian Ponce <damianemponce@gmail.com>
2022-10-14 17:55:32 -0300

    cambios4

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-addEventListener('mouseover', () => {
+addEventListener('click', () => {
     setTimeout(() => {
         document.getElementById('title').innerHTML= 'Joda mi rey, sos el mejor uwu';
     }, 2000);
